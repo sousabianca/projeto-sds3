20e26f0585e011752d7a70b7524005e8cd31325d
Bianca Sousa <biamoon19@gmail.com>
2021-09-07 17:19:57 -0300

    Basic stactic components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import NavBar from "components/NavBar";
+import DataTable from "components/NavBar/DataTable";
+import Footer from "components/NavBar/Footer";
 
 function App() {
   return (
-    <div >
-      <h1 className= "text-primary"> olá mundo!</h1>
-    </div>
-  );
+    <>
+      <NavBar />
+      <div className = 'container'>
+        <h1 className="text-primary"> olá mundo!</h1>
+     <DataTable/>
+      </div>
+      <Footer/>
+    </>
+        );
+
 }
 
-export default App;
+        export default App;
